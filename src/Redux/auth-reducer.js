2a3114f62f75dee799794faaa07bd6c4a0ae3491
BlishchAndrew <blishch.andrew@gmail.com>
2021-08-24 16:14:29 +0300

    add captcha

--- src/Redux/auth-reducer.js
+++ src/Redux/auth-reducer.js
@@ -1,18 +1,21 @@
 import { stopSubmit } from 'redux-form';
-import { authAPI } from '../api/api';
+import { authAPI, securityAPI } from '../api/api';
 
-const SET_USER_DATA = 'SET_USER_DATA';
+const SET_USER_DATA = 'SET_USER_DATA'; //samurai-network/auth/
+const GET_CAPTCHA_URL_SUCCESS = 'GET_CAPTCHA_URL_SUCCESS';
 
 let initialState = {
    userId: null,
    email: null,
    login: null,
    isAuth: false,
+	captchaUrl: null
 };
 
 const authReducer = (state = initialState, action) => {
    switch (action.type) {
       case SET_USER_DATA:
+      case GET_CAPTCHA_URL_SUCCESS:
          return { ...state, ...action.payload };
 
       default:
@@ -25,6 +28,11 @@ export const setAuthUserData = (userId, email, login, isAuth) => ({
    payload: { userId, email, login, isAuth },
 });
 
+export const getCaptchaUrlSuccess = (captchaUrl) => ({
+   type: 'GET_CAPTCHA_URL_SUCCESS',
+   payload: { captchaUrl },
+});
+
 export const getAuthUserData = () => async (dispatch) => {
 	const response = await authAPI.me();
 	
@@ -34,18 +42,26 @@ export const getAuthUserData = () => async (dispatch) => {
 	}
 }
 
-export const login = (email, password, rememberMe ) => async (dispatch) => {
-	const response = await authAPI.login(email, password, rememberMe);
+export const login = (email, password, rememberMe, captcha ) => async (dispatch) => {
+	const response = await authAPI.login(email, password, rememberMe, captcha);
 
 	if (response.data.resultCode === 0) {
 		dispatch(getAuthUserData());
 	} else {
+		if (response.data.resultCode === 10) {
+			dispatch(getCaptchaUrl());
+		}
 		let message =
 			response.data.messages.length > 0
 				? response.data.messages[0]
 				: 'Some error';
 		dispatch(stopSubmit('login', { _error: message }));
 	}
+}
+
+export const getCaptchaUrl = () => async (dispatch) => {
+	const response = await securityAPI.getCaptchaUrl();
+	dispatch(getCaptchaUrlSuccess(response.data.url));
 }
 
 export const logout = () => async (dispatch) => {
--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -8,12 +8,18 @@ import { connect } from 'react-redux';
 import { login } from '../../Redux/auth-reducer';
 import { Redirect } from 'react-router';
 
-const LoginForm = ({handleSubmit, error}) => {
+const LoginForm = ({handleSubmit, error, captchaUrl}) => {
    return (
       <form onSubmit={handleSubmit}>
 			{createField('Email', 'email', Input, [required, email])}
 			{createField('Password', 'password', Input, [required], {type: 'password'})}
 			{createField(null, 'rememberMe', Input, [], {type: 'checkbox'}, 'remember me')}
+			<div className={styleForm.captcha}>
+				{captchaUrl && (
+					<img src={captchaUrl} alt="" />
+				)}
+         	{captchaUrl && createField('captcha', 'captcha', Input, [required], {} )}
+			</div>
          {error && (
             <div className={styleForm.formSummaryError}>{error}</div>
          )}
@@ -26,7 +32,7 @@ const LoginReduxForm = reduxForm({ form: 'login' })(LoginForm);
 
 const Login = (props) => {
 	const onSubmit = (formData) => {
-		props.login(formData.email, formData.password, formData.rememberMe);
+		props.login(formData.email, formData.password, formData.rememberMe, formData.captcha);
 	}
 
 	if (props.isAuth) {
@@ -36,13 +42,14 @@ const Login = (props) => {
    return (
       <div>
          <h1 className={st.title}>Sign in to React Social Network</h1>
-         <LoginReduxForm onSubmit={onSubmit} />
+         <LoginReduxForm onSubmit={onSubmit} captchaUrl={props.captchaUrl} />
       </div>
    );
 };
 
 const mapStateToProps = (state) => ({
-	isAuth: state.auth.isAuth
+	isAuth: state.auth.isAuth,
+	captchaUrl: state.auth.captchaUrl
 })
 
 export default connect(mapStateToProps, {login}) (Login);
